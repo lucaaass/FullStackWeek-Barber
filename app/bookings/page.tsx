edc7b9c696790674683cc10498695b94bc844ecf
Lucas Marques <lucasmarques630@gmail.com>
2024-02-03 00:20:11 -0300

    corrigindo erro de tipagem

--- app/bookings/page.tsx
+++ app/bookings/page.tsx
@@ -5,6 +5,7 @@ import { redirect } from "next/navigation";
 import { db } from "../_lib/prisma";
 import BookingItem from "../_components/booking-item";
 
+
 const BookingsPage = async () => {
   const session = await getServerSession(authOptions);
 
@@ -51,7 +52,7 @@ const BookingsPage = async () => {
             <h2 className="text-gray-400 uppercase font-bold text-sm mb-3">Confirmados</h2>
 
             <div className="flex flex-col gap-3">
-              {confirmedBookings.map((booking) => (
+              {confirmedBookings.map((booking:any) => (
                 <BookingItem key={booking.id} booking={booking} />
               ))}
             </div>
@@ -63,7 +64,7 @@ const BookingsPage = async () => {
             <h2 className="text-gray-400 uppercase font-bold text-sm mt-6 mb-3">Finalizados</h2>
 
             <div className="flex flex-col gap-3">
-              {finishedBookings.map((booking) => (
+              {finishedBookings.map((booking:any) => (
                 <BookingItem key={booking.id} booking={booking} />
               ))}
             </div>
